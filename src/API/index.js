229cbbeacb5159490b85478e7cca1c85c816397b
unknown <meckenswyter@gmail.com>
2018-11-01 23:39:59 -0400

    added foursquareAPI helper logic

---
+++ src/API/index.js
@@ -0,0 +1,43 @@
+class Helper {
+  static baseUrl() {
+    return 'https://api.foursquare.com/v2';
+  }
+  static auth() {
+    const keys = {
+      client_id: 'WQD1L2ZACIORJHQCUJQZASDMGBW4AGP1U3OOWUT3GYOBV1LU',
+      client_secret: 'EB3WAOBPQKNAXKCVR3V0LIKUNHGSZLWZARI0EAROHNES12PI',
+      v: '20181101'
+    };
+    return Object.keys(keys).map(key => `${key}=${keys[key]}`).join('&');
+  }
+  static urlBuilder(urlPrams) {
+    if(!urlPrams) {
+      return '';
+    }
+    return Object.keys(urlPrams).map(key => `${key}=${urlPrams[key]}`).join('&');
+  }
+  static headers() {
+    return {
+      Accept: 'application.json'
+    };
+  }
+  static simpleFetch(endPoint,method,urlPrams) {
+    let requestInfo = {
+      method,
+      headers: Helper.headers()
+    };
+    return fetch(`${Helper.baseUrl()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(urlPrams)}`,requestInfo).then(res => res.json());
+  }
+}
+
+export default class FourSquareAPI {
+  static search(urlPrams) {
+    return Helper.simpleFetch('/venues/search', 'GET', urlPrams);
+  }
+  static venueDetails(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}`, 'GET');
+  }
+  static venuePhotos(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, 'GET');
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map'
+import FourSquareAPI from './API/'
 
 class App extends Component {
+
+  componentDidMount() {
+    FourSquareAPI.search({
+      near: 'Baltimore,MD',
+      query: 'Pizza',
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <Map />
