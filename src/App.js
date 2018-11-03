cddf363e6990eb2e44e100b662681efe03a674d0
unknown <meckenswyter@gmail.com>
2018-11-03 17:07:51 -0400

    added markers to map

--- src/App.js
+++ src/App.js
@@ -4,17 +4,40 @@ import Map from './components/Map'
 import FourSquareAPI from './API/'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      markers: [],
+      venues: [],
+      center: [],
+      zoom: 12
+    };
+  }
 
   componentDidMount() {
     FourSquareAPI.search({
       near: 'Cumberland,MD',
       query: 'Pizza',
       limit: 10
-    }).then(results => console.log(results));
+    }).then(results => {
+     const { venues } = results.response;
+     const { center } = results.response.geocode.feature.geometry;
+     const markers = venues.map(venue => {
+       return {
+         lat: venue.location.lat,
+         lng: venue.location.lng,
+         isOpen: false,
+         isShowing: true
+       };
+     });
+     this.setState({ markers, venues, center });
+     console.log(results);
+    });
   }
+
   render() {
     return (
-      <Map />
+      <Map {...this.state}/>
     );
   }
 }
